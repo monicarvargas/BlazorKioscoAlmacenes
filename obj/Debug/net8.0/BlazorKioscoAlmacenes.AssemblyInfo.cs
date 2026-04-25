e7f3b9ae0734b9dc8a66ee1e18640de1843b3f72
Monica <monica.r.vargas@gmail.com>
2026-04-25 13:42:17 -0300

    Es un cambio en index

--- obj/Debug/net8.0/BlazorKioscoAlmacenes.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorKioscoAlmacenes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorKioscoAlmacenes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3f915fcc45a63861e2f1c0124e6578c8db7ebe3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be2f3e13fe2c4e66facf0b3d7cf65e3fbf752880")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorKioscoAlmacenes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorKioscoAlmacenes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
