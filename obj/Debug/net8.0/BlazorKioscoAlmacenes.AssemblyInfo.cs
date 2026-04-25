c3f915fcc45a63861e2f1c0124e6578c8db7ebe3
Monica <monica.r.vargas@gmail.com>
2026-04-25 13:02:19 -0300

    Probando deploy con wwwroot

--- obj/Debug/net8.0/BlazorKioscoAlmacenes.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorKioscoAlmacenes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorKioscoAlmacenes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a916a77a1204c12a731dbd5829224dc5ef485048")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorKioscoAlmacenes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorKioscoAlmacenes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
